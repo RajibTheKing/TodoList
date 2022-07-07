ace97d3a461a9978a0a84dfaf585fc7dde309f2e
RajibTheKing <aporba.das@gmail.com>
2022-07-07 18:56:57 +0200

    remove extra subfolder from client

--- client/todo/src/store/todoData/getters.ts
+++
@@ -1,8 +0,0 @@
-import { GetterTree } from 'vuex'
-import { TodoState, Task, RootState } from '../../model/model'
-
-export const getters: GetterTree<TodoState, RootState> = {
-  getTaskList (state) : Task[] {
-    return state.items
-  }
-}
