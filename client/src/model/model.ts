4739f274d3d85d154626f664702ba5bf4b652bdb
RajibTheKing <aporba.das@gmail.com>
2022-07-07 18:57:24 +0200

    remove subfolder continued

---
+++ client/src/model/model.ts
@@ -0,0 +1,35 @@
+export interface Task {
+  id: string;
+  title: string;
+  status: string; // later we can improve this by enum
+  // priority: string, or we don't need this now
+}
+
+export type TodoState = {
+  loading: boolean;
+  items: Task[];
+};
+export const initialState: TodoState = {
+  loading: false,
+  items: [
+    {
+      id: '1',
+      title: 'Task 1',
+      status: 'PENDING'
+    },
+    {
+      id: '2',
+      title: 'Task 2',
+      status: 'DONE'
+    },
+    {
+      id: '3',
+      title: 'Task 3',
+      status: 'PENDING'
+    }
+  ]
+}
+
+export interface RootState {
+  version: string;
+}
